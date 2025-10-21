42853bb8f82861ffb1fe87484ffeca3ffef194d9
Paweł Rybak <87527837+wedkarz02@users.noreply.github.com>
2025-10-21 15:59:07 +0200

    added oil.nvim with git coloring

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -205,6 +205,9 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- Edit the parent directory of the current file as a buffer with Oil.nvim.
+vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
@@ -1032,6 +1035,28 @@ require('lazy').setup({
     config = true,
     -- use opts = {} for passing setup options
     -- this is equivalent to setup({}) function
+  },
+
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+      view_options = {
+        show_hidden = true,
+      },
+    },
+    -- Optional dependencies
+    dependencies = { { 'nvim-mini/mini.icons', opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+  },
+
+  {
+    'benomahony/oil-git.nvim',
+    dependencies = { 'stevearc/oil.nvim' },
+    -- No opts or config needed! Works automatically
   },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
