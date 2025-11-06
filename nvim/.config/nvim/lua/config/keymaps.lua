38f1074e73f88fa45963fac472e12e82b74bed23
Paweł Rybak <87527837+wedkarz02@users.noreply.github.com>
2025-11-06 12:29:09 +0100

    readded keymap to open oil.nvim in a floating window

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -36,3 +36,10 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+
+vim.keymap.set(
+  'n',
+  '-',
+  '<CMD>lua require("oil").open_float(".", { preview = { vertical = true }})<CR>',
+  { desc = 'Open parent directory in a floating buffer' }
+)
