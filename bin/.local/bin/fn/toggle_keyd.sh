515fd9f2a0a66c5c765af7ead728ef2a9fa4fe01
Paweł Rybak <87527837+wedkarz02@users.noreply.github.com>
2025-11-11 14:21:09 +0100

    script for toggling keyd service

---
+++ bin/.local/bin/fn/toggle_keyd.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Add this script as a system shortcut and bind
+# to some key combination.
+# At the moment I use `Meta+Shift+K`.
+
+if [[ "$(systemctl show -p SubState --value keyd)" == "running" ]]; then
+	if pkexec systemctl stop keyd; then
+		notify-send "Toggle keyd" "Stopped keyd."
+	else
+		notify-send "Failed to stop keyd" "Permission denied or service error."
+		exit 1
+	fi
+else
+	if pkexec systemctl start --now keyd; then
+		notify-send "Toggle keyd" "Started keyd."
+	else
+		notify-send "Failed to start keyd" "Permission denied or service error."
+		exit 1
+	fi
+fi
+
