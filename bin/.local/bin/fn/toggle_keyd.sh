45f91e6b87055aaef3567fe7970b26b7a86f22cb
Paweł Rybak <87527837+wedkarz02@users.noreply.github.com>
2026-01-03 17:07:33 +0100

    removed notification on toggle-keyd success

--- bin/.local/bin/fn/toggle_keyd.sh
+++ bin/.local/bin/fn/toggle_keyd.sh
@@ -5,18 +5,14 @@
 # At the moment I use `Meta+Shift+K`.
 
 if [[ "$(systemctl show -p SubState --value keyd)" == "running" ]]; then
-	if pkexec systemctl stop keyd; then
-		notify-send "Toggle keyd" "Stopped keyd."
-	else
-		notify-send "Failed to stop keyd" "Permission denied or service error."
-		exit 1
-	fi
+  if ! pkexec systemctl stop keyd; then
+    notify-send "Failed to stop keyd" "Permission denied or service error."
+    exit 1
+  fi
 else
-	if pkexec systemctl start --now keyd; then
-		notify-send "Toggle keyd" "Started keyd."
-	else
-		notify-send "Failed to start keyd" "Permission denied or service error."
-		exit 1
-	fi
+  if ! pkexec systemctl start --now keyd; then
+    notify-send "Failed to start keyd" "Permission denied or service error."
+    exit 1
+  fi
 fi
 
